f78a89edb7f97abc9559741f6273bd1654db20c6
nostrss <jintagi@gmail.com>
2022-03-23 12:55:14 +0900

    0323 class

---
+++ class/src/components/units/board/08-board-write/BoardWrite.container.js
@@ -0,0 +1,85 @@
+//컨테이너 컴포넌트
+import BoardWriteUI from './BoardWrite.presenter'
+import { useMutation } from '@apollo/client'
+import { useState } from 'react'
+import { CREAT_BOARD, UPDATE_BOARD } from './BoardWrite.queries'
+import { useRouter} from 'next/router'
+
+export default function BoardWrite(props) {
+  const [data, setData] = useState('')
+  const [myWriter, setMyWriter] = useState('')
+  const [myTitle, setMyTitle] = useState('')
+  const [myContents, setMyContents] = useState('')
+  const [isActive, setIsActive] = useState('')
+  const [callApi] = useMutation(CREAT_BOARD)
+  const [updateBoard] = useMutation(UPDATE_BOARD)
+  const router = useRouter()
+
+  const onClickUpdate = async () => {
+    const result = await updateBoard({
+      variables: { number: Number(router.query.mynumber) , writer: myWriter, title: myTitle, contents: myContents
+        
+      }
+    })
+
+    alert('게시글 수정에 성공하였습니다')
+    router.push(`/08-05-boards/${router.query.mynumber}`)
+
+  }
+
+  const callGraphqlApi = async () => {
+    const result = await callApi({
+      variables: { writer: myWriter, title: myTitle, contents: myContents },
+    })
+    console.log(result)
+    setData(result.data.createBoard.message)
+
+
+    alert('게시글 등록이 성공하였습니다')
+    router.push(`/08-05-boards/${result.data.createBoard.number}`)
+  }
+
+  const onChangeWriter = (event) => {
+    setMyWriter(event.target.value)
+    if (event.target.value !== "" && myTitle !== "" && myContents !== "")
+    {
+      setIsActive(true)
+    } else {
+      setIsActive(false)
+    }
+  }
+
+  const onChangeTitle = (event) => {
+    setMyTitle(event.target.value)
+    if (myWriter !== "" && event.target.value !== "" && myContents !== "")
+    {
+      setIsActive(true)
+    } else {
+      setIsActive(false)
+    }
+  }
+
+  const onChangeContents = (event) => {
+    setMyContents(event.target.value)
+    if (myWriter !== "" && myTitle !== "" && event.target.value!== "")
+    {
+      setIsActive(true)
+    } else {
+      setIsActive(false)
+    }
+  }
+
+
+  return (
+    <BoardWriteUI
+      onChangeWriter={onChangeWriter}
+      onChangeTitle={onChangeTitle}
+      onChangeContents={onChangeContents}
+      callGraphqlApi={callGraphqlApi}
+      onClickUpdate={onClickUpdate}
+      isActive={isActive}
+      isEdit={props.isEdit}
+    />
+    
+  )
+}
---
+++ class/src/components/units/board/08-board-write/BoardWrite.style.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+
+export const SubmitButton = styled.button`
+  background-color: ${(props)=>props.isActive ? "yellow" : "gray"};
+`
+export const WriterInput = styled.input`
+  border-color: green;
+`
+
