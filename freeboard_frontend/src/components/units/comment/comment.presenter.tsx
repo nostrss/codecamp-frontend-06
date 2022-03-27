9fff31acc7794641994752f9c8a8e78d47f9c434
nostrss <jintagi@gmail.com>
2022-03-27 20:03:48 +0900

    CF-3 댓글 작성하기

--- freeboard_frontend/src/components/units/comment/comment.presenter.tsx
+++ freeboard_frontend/src/components/units/comment/comment.presenter.tsx
@@ -46,13 +46,15 @@ export default function PostCommentUI(props: IFetchCommentData) {
           </P.CommentInfo>
           <P.TextareaWrap>
             <P.CommentContents
+              onChange={props.onChangeComment}
               maxLength={props.textLimit}
               placeholder='개인정보를 공유 및 요청하거나, 명예 훼손, 무단 광고, 불법 정보 유포시 모니터링 후 삭제될 수 있으며, 이에 대한 민형사상 책임은 게시자에게 있습니다.'
-              onChange={props.onChangeComment}
             ></P.CommentContents>
             <P.RowSpaceBetween>
               <P.CountTextLength>0/100</P.CountTextLength>
-              <P.SubmitComment>등록하기</P.SubmitComment>
+              <P.SubmitComment onClick={props.onClickSubmitComment}>
+                등록하기
+              </P.SubmitComment>
             </P.RowSpaceBetween>
           </P.TextareaWrap>
         </ColumnWrapper>
