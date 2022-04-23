94511ecb3bb8086209dac8517c486cbdda023589
nostrss <jintagi@gmail.com>
2022-04-23 21:30:34 +0900

    CF-64

--- freeboard_frontend/src/components/units/comment2/list/BoardCommentList.presenter.tsx
+++ freeboard_frontend/src/components/units/comment2/list/BoardCommentList.presenter.tsx
@@ -1,13 +1,14 @@
-import BoardCommentListUIItem from "./BoardCommentList.presenterItem";
-import { IBoardCommentListUIProps } from "./BoardCommentList.types";
-import InfiniteScroll from "react-infinite-scroller";
+import BoardCommentListUIItem from './BoardCommentList.presenterItem';
+import { IBoardCommentListUIProps } from './BoardCommentList.types';
+import InfiniteScroll from 'react-infinite-scroller';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function BoardCommentListUI(props: IBoardCommentListUIProps) {
   if (!props.data) return <div />;
   return (
     <InfiniteScroll pageStart={0} loadMore={props.onLoadMore} hasMore={true}>
       {props.data?.fetchBoardComments.map((el) => (
-        <BoardCommentListUIItem key={el._id} el={el} />
+        <BoardCommentListUIItem key={String(uuidv4())} el={el} />
       ))}
     </InfiniteScroll>
   );
