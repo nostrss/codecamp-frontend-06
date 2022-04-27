d18ac11eac224d889036768cf3eebf5acdb79a88
nostrss <jintagi@gmail.com>
2022-04-28 00:50:39 +0900

    상품리스트탭

--- freeboard_frontend/src/components/units/productlist/productlist.queries.ts
+++ freeboard_frontend/src/components/units/productlist/productlist.queries.ts
@@ -1,26 +1,8 @@
-// fetchUseditems(
-//   isSoldout: Boolean
-//   search: String
-//   page: Int
-//   ): [Useditem!]!
-
-// query{
-//   fetchUseditems{
-//     _id
-//     name
-//     remarks
-//     price
-//     tags
-//     pickedCount
-//     seller  {_id,name}
-//     }
-// }
-
 import { gql } from '@apollo/client';
 
 export const FETCH_PRODUCTS = gql`
-  query fetchUseditems($page: Int) {
-    fetchUseditems(page: $page) {
+  query fetchUseditems($page: Int, $isSoldout: Boolean) {
+    fetchUseditems(page: $page, isSoldout: $isSoldout) {
       _id
       name
       remarks
